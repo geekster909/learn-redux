8ad350683bc8c7a7855dc475100469ec912e2f10
Justin Bond <bond.justink@gmail.com>
2018-01-08 16:49:53 -0800

    Added js files, called main.js

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,23 @@
-// let's go!
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+//import css
+import css from './styles/style.styl';
+
+//import componenets
+import Main from './components/Main';
+
+class Reduxstagram extends Component {
+	render() {
+		return (
+			<div>
+				<p>Hi</p>
+			</div>
+		);
+	}
+}
+
+render(
+	<Main />,
+	document.getElementById('root')
+);
